72e8d867ba0a4e7d5cc7946a797a78d4a16a2198
unknown <ashuanand8804@gmail.com>
2026-07-05 10:54:02 +0530

    revision ongoing

--- 1_Verilog_Language/4_Procedures/03_IfStatement.v
+++ 1_Verilog_Language/4_Procedures/03_IfStatement.v
@@ -6,5 +6,14 @@ module top_module(
     input sel_b2,
     output wire out_assign,
     output reg out_always   ); 
-
+	
+    always @(*) begin
+        if (sel_b1 && sel_b2)
+            out_always = b;
+    	else
+            out_always = a;
+    end
+    
+    assign out_assign = (sel_b1 && sel_b2) ? b:a;
+    
 endmodule
